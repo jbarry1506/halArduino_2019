c662d20746c2b1fec7ee51352c6201d59347de14
Jim Barry <jbarry1506@gmail.com>
2019-09-29 20:59:54 -0400

    added base image function

--- webcam_spike.py
+++ webcam_spike.py
@@ -3,12 +3,32 @@ import cv2
 import serial
 import time
 
-ser = serial.Serial('COM3', 9600)
-cap = cv2.VideoCapture(0)
+
+# ser = serial.Serial('COM3', 9600)
+
+# function to get the initial image to compare for motion detection
+def get_base_image(camera):
+    retimage, im = camera.read()
+    return im
+
+
+# camera 1 is the external camera
+cap = cv2.VideoCapture(1)
+# capture the base image for comparison
+base_image = get_base_image(cap)
+# set up the video codec
+fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+# set up the video to be saved
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640,480))
+# save the base image
+cv2.imwrite('baseimage.jpg', base_image)
+
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
+
+    out.write(frame)
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -19,10 +39,11 @@ while(True):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    ser.write(b'H')
-    time.sleep(1)
-    ser.write(b'L')
-    time.sleep(1)
+    # ser.write(b'H')
+    # time.sleep(1)
+    # ser.write(b'L')
+    # time.sleep(1)
 # When everything done, release the capture
 cap.release()
+out.release()
 cv2.destroyAllWindows()
